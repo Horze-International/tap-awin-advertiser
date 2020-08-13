79b0cf454a104854bcbcf1b9a13eceae6c9d664f
Leonhard Schick <leonhard.schick@horze.com>
2020-08-13 11:01:23 +0200

    fix stream transactions make click_device nullable

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-awin-advertiser',
-      version="0.1.5",
+      version="0.1.6",
       description='Singer.io tap for extracting data from the AWIN Advertiser API',
       author='Horze',
       url='http://horze.de',
