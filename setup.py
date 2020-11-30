ee33f4576d40dbbb6d11ec6066b24336a5a4ffb8
Leonhard Schick <leonhard.schick@horze.com>
2020-11-30 15:24:06 +0100

    bump version 0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-awin-advertiser',
-      version="0.2.0",
+      version="0.2.1",
       description='Singer.io tap for extracting data from the AWIN Advertiser API',
       author='Horze',
       url='http://horze.de',
