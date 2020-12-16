ea1cb8040b552372c7f5ba142b3ef5c9ad9b07e3
Leonhard Schick <leonhard.schick@horze.com>
2020-12-16 12:28:33 +0100

    request next data when result is empty

--- tap_awin_advertiser/sync.py
+++ tap_awin_advertiser/sync.py
@@ -214,110 +214,107 @@ def sync_endpoint(
         time_extracted = utils.now()
         if not data or data is None or data == {}:
             LOGGER.info('No data results returned')
-            total_records = 0
-            break # No data results
-
-        # Transform data with transform_json from transform.py
-        # The data_key_array identifies the array/list of records below the <root> element
-        # LOGGER.info('data = {}'.format(data)) # TESTING, comment out
-        transformed_data = [] # initialize the record list
-
-        if data_key_array:
-            data_records = data.get(data_key_array, [])
         else:
-            data_records = data
+            # Transform data with transform_json from transform.py
+            # The data_key_array identifies the array/list of records below the <root> element
+            # LOGGER.info('data = {}'.format(data)) # TESTING, comment out
+            transformed_data = [] # initialize the record list
 
-        for record in data_records:
-            # Add parent id field/value
-            if parent and parent_id and parent not in record:
-                record[parent] = parent_id
+            if data_key_array:
+                data_records = data.get(data_key_array, [])
+            else:
+                data_records = data
 
-            # transform record (remove inconsistent use of CamelCase)
-            try:
-                transformed_record = humps.decamelize(record)
-            except Exception as err:
-                LOGGER.error('{}'.format(err))
-                LOGGER.error('error record: {}'.format(record))
-                raise Exception(err)
+            for record in data_records:
+                # Add parent id field/value
+                if parent and parent_id and parent not in record:
+                    record[parent] = parent_id
 
-            transformed_data.append(transformed_record)
-            # End for record in array
-        # End non-stats stream
+                # transform record (remove inconsistent use of CamelCase)
+                try:
+                    transformed_record = humps.decamelize(record)
+                except Exception as err:
+                    LOGGER.error('{}'.format(err))
+                    LOGGER.error('error record: {}'.format(record))
+                    raise Exception(err)
 
-        # LOGGER.info('transformed_data = {}'.format(transformed_data)) # COMMENT OUT
-        if not transformed_data or transformed_data is None:
-            LOGGER.info('No transformed data for data = {}'.format(data))
-            total_records = 0
-            break # No transformed_data results
+                transformed_data.append(transformed_record)
+                # End for record in array
+            # End non-stats stream
 
-        # Process records and get the max_bookmark_value and record_count
-        if stream_name in sync_streams:
-            max_bookmark_value, record_count = process_records(
-                catalog=catalog,
-                stream_name=stream_name,
-                records=transformed_data,
-                time_extracted=time_extracted,
-                bookmark_field=bookmark_field,
-                max_bookmark_value=max_bookmark_value,
-                last_datetime=last_datetime)
-            LOGGER.info('Stream {}, batch processed {} records'.format(
-                stream_name, record_count))
+            # LOGGER.info('transformed_data = {}'.format(transformed_data)) # COMMENT OUT
+            if not transformed_data or transformed_data is None:
+                LOGGER.info('No transformed data for data = {}'.format(data))
+            else:
 
-        # Loop thru parent batch records for each children objects (if should stream)
-        children = endpoint_config.get('children')
-        if children:
-            for child_stream_name, child_endpoint_config in children.items():
-                if child_stream_name in sync_streams:
-                    LOGGER.info('START Syncing: {}'.format(child_stream_name))
-                    write_schema(catalog, child_stream_name)
-                    # For each parent record
-                    for record in transformed_data:
-                        i = 0
-                        # Set parent_id
-                        for id_field in id_fields:
-                            if i == 0:
-                                parent_id_field = id_field
-                            if id_field == 'id':
-                                parent_id_field = id_field
-                            i = i + 1
-                        parent_id = record.get(parent_id_field)
+                # Process records and get the max_bookmark_value and record_count
+                if stream_name in sync_streams:
+                    max_bookmark_value, record_count = process_records(
+                        catalog=catalog,
+                        stream_name=stream_name,
+                        records=transformed_data,
+                        time_extracted=time_extracted,
+                        bookmark_field=bookmark_field,
+                        max_bookmark_value=max_bookmark_value,
+                        last_datetime=last_datetime)
+                    LOGGER.info('Stream {}, batch processed {} records'.format(
+                        stream_name, record_count))
 
-                        # sync_endpoint for child
-                        LOGGER.info(
-                            'START Sync for Stream: {}, parent_stream: {}, parent_id: {}'\
-                                .format(child_stream_name, stream_name, parent_id))
+                # Loop thru parent batch records for each children objects (if should stream)
+                children = endpoint_config.get('children')
+                if children:
+                    for child_stream_name, child_endpoint_config in children.items():
+                        if child_stream_name in sync_streams:
+                            LOGGER.info('START Syncing: {}'.format(child_stream_name))
+                            write_schema(catalog, child_stream_name)
+                            # For each parent record
+                            for record in transformed_data:
+                                i = 0
+                                # Set parent_id
+                                for id_field in id_fields:
+                                    if i == 0:
+                                        parent_id_field = id_field
+                                    if id_field == 'id':
+                                        parent_id_field = id_field
+                                    i = i + 1
+                                parent_id = record.get(parent_id_field)
 
-                        child_total_records = sync_endpoint(
-                            client=client,
-                            config=config,
-                            catalog=catalog,
-                            state=state,
-                            stream_name=child_stream_name,
-                            endpoint_config=child_endpoint_config,
-                            sync_streams=sync_streams,
-                            selected_streams=selected_streams,
-                            parent_id=parent_id)
+                                # sync_endpoint for child
+                                LOGGER.info(
+                                    'START Sync for Stream: {}, parent_stream: {}, parent_id: {}'\
+                                        .format(child_stream_name, stream_name, parent_id))
 
-                        LOGGER.info(
-                            'FINISHED Sync for Stream: {}, parent_id: {}, total_records: {}'\
-                                .format(child_stream_name, parent_id, child_total_records))
-                        # End transformed data record loop
-                    # End if child in sync_streams
-                # End child streams for parent
-            # End if children
+                                child_total_records = sync_endpoint(
+                                    client=client,
+                                    config=config,
+                                    catalog=catalog,
+                                    state=state,
+                                    stream_name=child_stream_name,
+                                    endpoint_config=child_endpoint_config,
+                                    sync_streams=sync_streams,
+                                    selected_streams=selected_streams,
+                                    parent_id=parent_id)
 
-        # Parent record batch
-        total_records = total_records + record_count
-        endpoint_total = endpoint_total + record_count
+                                LOGGER.info(
+                                    'FINISHED Sync for Stream: {}, parent_id: {}, total_records: {}'\
+                                        .format(child_stream_name, parent_id, child_total_records))
+                                # End transformed data record loop
+                            # End if child in sync_streams
+                        # End child streams for parent
+                    # End if children
 
-        LOGGER.info('Synced Stream: {}, records: {}'.format(
-            stream_name,
-            total_records))
+                # Parent record batch
+                total_records = total_records + record_count
+                endpoint_total = endpoint_total + record_count
 
-        # Update the state with the max_bookmark_value for the stream date window
-        # Snapchat Ads API does not allow page/batch sorting; bookmark written for date window
-        if bookmark_field and stream_name in selected_streams:
-            write_bookmark(state, stream_name, max_bookmark_value, bookmark_field, parent, parent_id)
+                LOGGER.info('Synced Stream: {}, records: {}'.format(
+                    stream_name,
+                    total_records))
+
+                # Update the state with the max_bookmark_value for the stream date window
+                # Snapchat Ads API does not allow page/batch sorting; bookmark written for date window
+                if bookmark_field and stream_name in selected_streams:
+                    write_bookmark(state, stream_name, max_bookmark_value, bookmark_field, parent, parent_id)
 
         # Increment date window and sum endpoint_total
         start_window = end_window + timedelta(days=1)
